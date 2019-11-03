ed997f79dfa3e0a260d101330f8c16973e71239a
Edwin <edwincruz25@unitec.edu>
2019-11-03 17:05:20 -0600

    added loader

--- src/components/ExcelLoader.js
+++ src/components/ExcelLoader.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { OutTable, ExcelRenderer } from 'react-excel-renderer';
 
 const ExcelLoader = () => {
     const [columns, setColumns] = useState([]);
     const [rows, setRows] = useState([]);
+    const [data, setData] = useState([]);
     const [isPredictionReady, setIsPredictionReady] = useState(false);
 
     const fileHandler = (event) => {
@@ -13,22 +14,30 @@ const ExcelLoader = () => {
                 console.log(err);
             }
             else {
-                console.log(resp)
-                setRows(resp.rows.shift());
-                setColumns(resp.rows[0].map((col, index) => ({ name: col, key: index })));
+                const dataColumns = resp.rows[0].map((col, index) => ({ name: col, key: index + 1 }));
+                resp.rows.shift()
+                const dataRows = resp.rows;
+                const data = dataRows.map(row => {
+                    const newRow = dataColumns.map(column => ({ [column.name]: row[column.key - 1] }))
+                    debugger
+                })
+
+                setColumns(dataColumns);
+                resp.rows.shift()
+                setRows(resp.rows);
             }
         });
 
     }
-    const sendFileToPredict = async () => {
+    const sendFileToPredict = () => {
         setIsPredictionReady(true);
-        const response = await fetch('')
+
     }
     return <div>
         <input type="file" onChange={fileHandler} style={{ "padding": "10px" }} />
         <input type="checkbox" label="hasHeaders"></input>
         <OutTable data={rows} columns={columns} tableClassName="ExcelTable2007" tableHeaderRowClass="heading" />
-        <button onClick={sendFileToPredict} />Save</button>
+        <button onClick={sendFileToPredict}>Save</button>
     </div >
 }
 
